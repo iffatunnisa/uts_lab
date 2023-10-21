93b6f8f3555221ffdf36184ec68bc343dbf52e48
iffatunnisa <127869077+iffatunnisa@users.noreply.github.com>
2023-10-22 00:03:10 +0700

    Create Bunga.java

---
+++ Bunga.java
@@ -0,0 +1,13 @@
+abstract class Bunga {
+    private String namaBunga;
+    private String warnaBunga;
+    private String aromaBunga;
+    private String namaIlmiah;
+
+    // abstract
+    public abstract void tumbuh();
+
+    // constructor    
+    public Bunga() {
+
+    }
